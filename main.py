d8f147b96c29752b7cadb09b9b5e5e4a8639f1c1
784463729@qq.com <39975175+PlumeGu@users.noreply.github.com>
2019-01-11 09:16:45 +0800

    算法异常待解决

--- main.py
+++ main.py
@@ -7,11 +7,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from prettytable import PrettyTable
-from Downlink import test_fun
+#from Downlink import test_fun
 
 # ***参数配置表*** #
 # |数量定义|
-num_node = 1500 # 节点数量
+num_node = 1000 # 节点数量
 num_gw = 5  # 网关数量
 
 # |随机分布类型|
@@ -19,7 +19,7 @@ random_type = 'uniform'  # 随机类型（高斯分布or均匀分布 gaussian/un
 algorithm_type = '3'       # 多路径下行算法（1.仅信号质量 2.得分加权求和 3.）
 
 # |网关参数|
-gw_para = np.array([[0, 28], [0, 37], [0, 124], [0, 24], [0, 22]], dtype=np.float)  # 网关参数列表[网关负载%, 网络延时ms]
+gw_para = np.array([[1, 0], [1, 0], [1, 0], [1, 0], [1, 0]], dtype=np.float)  # 网关负载列表[上行负载%, 下行负载%]
 
 # ******end****** #
 
@@ -36,6 +36,8 @@ ping = np.zeros((num_gw, 1))
 load = np.zeros((num_gw, 1))
 score = np.zeros((num_node,num_gw))
 P_snr = np.zeros((num_node,num_gw))
+snr_good = [0]
+snr_good = snr_good * num_gw
 color = []
 color_dot = ['y.', 'g.', 'm.', 'b.', 'c.']
 colors = ['r', 'y', 'g', 'b', 'r', 'y', 'g', 'b', 'r']
@@ -54,12 +56,14 @@ if random_type == 'gaussian':
     GwList[:, 0] = gw_x
     GwList[:, 1] = gw_y
 
+
 # 均匀分布
 if random_type == 'uniform':
     # 生成节点列表
     NodeList = np.random.sample((num_node, 2))  # 生成x行y列的随机数组  均匀分布
     node_x = NodeList[:, 0]  # 取第一列
     node_y = NodeList[:, 1]  # 取第二列
+    node_SF = np.random.randint(7, 13, size=num_node)  # 随机生成SF，范围[7,12]
     # 生成网关列表
     GwList = np.random.sample((num_gw, 2))
     gw_x = GwList[:, 0]
@@ -110,7 +114,7 @@ if algorithm_type == '2':
         ax1.plot(node_x[i], node_y[i], color[i])  # 绘制节点
 
         #                       **** 计算动态网关负载 ****
-        gw_para[bestgw][0] += 0.1
+        gw_para[bestgw][1] += 0.1
 
 # 算法3：自创算法
 if algorithm_type == '3':
@@ -118,17 +122,18 @@ if algorithm_type == '3':
         #                          **** 获取输入参数 ****
         # 信号质量
         dist = np.sqrt(np.sum(np.square(GwList - NodeList[i]), axis=1))  # 二维坐标距离计算
-        # 网络延时
-        ping = gw_para[:, 1]
-        # 网关负载
-        load = gw_para[:, 0]
+        # 上行负载
+        load_up = gw_para[:, 0]
+        # 下行负载
+        load_down = gw_para[:, 1]
         #                         **** 数据限幅 ****
         for j in range(0, num_gw, 1):
-            if (load[j] >= 100): load[j] = 100
-            if (ping[j] >= 5000): ping[j] = 5000
+            if (load_up[j] >= 100): load[j] = 100
+            if (load_down[j] >= 100): ping[j] = 100
 
         #                      **** 坐标距离 -> SNR转换 ****
-        snr1 = (dist * 30 / np.sqrt(2)) - 15  # 坐标距离转换为SNR,范围[-15，+15]
+        snr1 = -((dist * 30 / np.sqrt(2)) - 15)  # 坐标距离转换为SNR,范围[-15,+15]
+        snr1 = snr1.tolist()
 
         #                       **** 分布执行算法 ****
         # 1、通过Ping的限制筛选网关
@@ -137,13 +142,48 @@ if algorithm_type == '3':
 
         # 3、在信号质量优的网关中，上下行占空比相加，择小者下行
 
+
+
+        pingcnt = 0
+        for j in range(0, num_gw, 1):
+            if(ping[j] >= 450):
+                pingcnt += 1
+
+        # 若不存在Ping合理的网关
+        if(pingcnt >= num_gw):
+            print("no GW with acceptable Ping")
+
+        # 若存在Ping合理 可下行的网关
+        else:
+            snrcnt = 0
+            for j in range(0, num_gw, 1):
+                if (dist[j] >= 0.2):
+                    snr_good[j] = 100
+                    snrcnt += 1
+                else:
+                    snr_good[j] = 0
+
+            # 若SNR较好 的网关 数量小于等于1
+            if (snrcnt >= num_gw-1):
+                bestgw = snr1.index(min(snr1))
+
+            # 若存在多个SNR较好的网关,选择负载和最小者下行
+            else:
+                load = (load_up + load_down)+snr_good
+                load = load.tolist()
+                bestgw = load.index(min(load))
+
+        color.append(color_dot[bestgw])  # 生成颜色列表
+        ax1.plot(node_x[i], node_y[i], color[i])  # 绘制节点
+
+        #                       **** 计算动态网关负载 ****
+        gw_para[bestgw][1] += 0.1
+
+'''
         P_snr = 1-(0.00000001046 * pow(3.825, -snr1))  # 带入线性回归曲线
 
         # 根据Ping计算成功率
-        if(ping1 <= 450 ):
-            P_ping = 1
-        else:
-            P_ping = 0
+
 
         # 根据Load计算成功率
 
@@ -153,10 +193,8 @@ if algorithm_type == '3':
         bestgw = P_all_list.index(min(P_all_list))
         color.append(color_dot[bestgw])  # 生成颜色列表
         ax1.plot(node_x[i], node_y[i], color[i])  # 绘制节点
+'''
 
-
-        #                       **** 计算动态网关负载 ****
-        gw_para[bestgw][0] += 0.1
 
 
 # 绘制网关
@@ -167,7 +205,7 @@ plt.show()
 
 
 # 打印网关参数表
-print_table = PrettyTable(["网关编号", "负载(%)", "网络延时(ms)"])  # Title
+print_table = PrettyTable(["网关编号", "上行负载(%)", "下行负载(%)"])  # Title
 for i in range(0, 5):
     print_table.add_row([i+1, gw_para[i][0], gw_para[i][1]])
 print(print_table)
